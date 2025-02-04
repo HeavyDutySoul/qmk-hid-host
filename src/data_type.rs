574266ebfc0c34bd680ad4e3df5af25b48ff05ed
HeavyDutySoul <putsli@gmail.com>
2025-02-04 16:59:19 +0300

    Delete unused data.

--- src/data_type.rs
+++ src/data_type.rs
@@ -1,10 +1,10 @@
 #[cfg(not(target_os = "macos"))]
 pub enum DataType {
-    Time = 0xAA, // random value that does not conflict with VIA/VIAL, must match firmware
-    Volume,
+    //Time = 0xAA, // random value that does not conflict with VIA/VIAL, must match firmware
+    //Volume,
     Layout,
-    MediaArtist,
-    MediaTitle,
+    //MediaArtist,
+    //MediaTitle,
 
     RelayFromDevice = 0xCC,
     RelayToDevice,
